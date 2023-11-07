c70458e929820c4811a26c8376c03778b13b6e2e
Edvin Amborg <118813643+edvinamborg@users.noreply.github.com>
2023-11-07 12:26:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js');
+let registration = null;
+
+function register_service__worker() {
+	if ("serviceWorker" in navigator) {
+		window.navigator.serviceWorker
+			.register("./service-worker.js", { scope: "./" })
+			.then((res) => {
+				registration = res;
+				console.log("Service worker successfully registerd");
+			})
+			.catch((err) => {
+				console.log("Service worker not registerd");
+			});
+	}
 }
+
+function unregister_service__worker() {
+	navigator.serviceWorker
+		.getRegistrations()
+		.then((registrations) => {
+			registrations.forEach((registration) => {
+				registration.unregister();
+				console.log("Service worker unregistered");
+			});
+		})
+		.catch((err) => {
+			console.log("could not register service worker");
+		});
+}
+
+register_service__worker();
