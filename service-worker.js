a45896483aecfa957ca7f93cd76adb69d6c3db53
Edvin Amborg <118813643+edvinamborg@users.noreply.github.com>
2023-11-07 12:27:10 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
-let registration = null;
-
-function register_service__worker() {
-	if ("serviceWorker" in navigator) {
-		window.navigator.serviceWorker
-			.register("./service-worker.js", { scope: "./" })
-			.then((res) => {
-				registration = res;
-				console.log("Service worker successfully registerd");
-			})
-			.catch((err) => {
-				console.log("Service worker not registerd");
-			});
-	}
-}
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('v1')
+            .then(cache => {
+                cache.addAll([
+                    './',
+                    './style.css',
+                    './logic.js'
+                ]);
+                console.log('Assets cached')
+                
+            })
+            .catch(err => console.log('could not cache assets')) 
+    )  
+})
